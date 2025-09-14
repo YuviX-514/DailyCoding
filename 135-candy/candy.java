a35ebeea3a67a901ecf6e8a2fad29db1ec702afe
Yuvraj Singh <yuvi4695@gmail.com>
2025-09-14 21:47:34 +0530

    Time: 3 ms (79.69%) | Memory: 46 MB (33.91%) - LeetSync

---
+++ 135-candy/candy.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int [] candies = new int[ratings.length];
+        for(int i =0;i<candies.length;i++){
+            candies[i] = 1;
+        }
+
+        //left :
+        for(int i =1;i<ratings.length;i++){
+            if(ratings[i]>ratings[i-1]){
+                candies[i] = candies[i-1] +1;
+            }
+        }
+        //right :
+        for(int i =ratings.length-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1]){
+                candies[i] = Math.max(candies[i], candies[i+1]+1);
+            }
+        }
+        int count = 0;
+        for(int i =0;i<candies.length;i++){
+            count += candies[i];
+        }
+        return count;
+    }
+}
