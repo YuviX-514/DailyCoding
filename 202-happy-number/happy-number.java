3e019d07fbba294c5aa6ebf4edd50acb4e6a77dd
Yuvraj Singh <yuvi4695@gmail.com>
2025-09-18 23:10:59 +0530

    Time: 2 ms (17.44%) | Memory: 40.9 MB (51.51%) - LeetSync

---
+++ 202-happy-number/happy-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> set = new HashSet<>();
+        
+        while (n != 1 && !set.contains(n)) {
+            set.add(n);
+            n = cycleCal(n);
+        }
+        //System.out.print(set);
+        return n == 1;
+    }
+    private int cycleCal(int n){
+        int sum = 0;
+        while(n > 0){
+            int digit = n % 10;
+            n = n / 10;
+            sum = sum + digit*digit;
+        }
+        return sum;
+    }
+}
