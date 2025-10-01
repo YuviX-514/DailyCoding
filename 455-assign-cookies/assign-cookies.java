0ccb9901601beb945fd70c343bb04ba87ef5f127
Yuvraj Singh <yuvi4695@gmail.com>
2025-10-01 10:51:18 +0530

    Time: 11 ms (35.76%) | Memory: 48.4 MB (85.08%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        int i  = 0;
+        int j = 0;
+        int c  = 0;
+        Arrays.sort(g);
+        Arrays.sort(s);
+        while(i < g.length && j < s.length){
+            if(g[i] <= s[j]){
+                c++;
+                i++;
+                j++;
+            }
+            else j++;
+        }
+        return c;
+    }
+}
