ba00083c39afdbb87a8142fdfda80efa3a0822b0
Yuvraj Singh <yuvi4695@gmail.com>
2025-10-06 02:07:41 +0530

    Time: 692 ms (5.09%) | Memory: 55.4 MB (84.72%) - LeetSync

---
+++ 2366-maximum-bags-with-full-capacity-of-rocks/maximum-bags-with-full-capacity-of-rocks.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int maximumBags(int[] capacity, int[] rocks, int ar) {
+        int n = rocks.length;
+        Pair[] arr = new Pair[n];
+        for(int i = 0;i < n;i++){
+            arr[i] = new Pair(capacity[i], rocks[i]);
+        }
+        Arrays.sort(arr, (a, b)->(Integer.compare((a.c - a.r), (b.c-b.r))));
+
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i].c+":"+arr[i].r+ " ");
+        }
+        int op = 0;
+        for(int i = 0;i<n;i++){
+            int ch = arr[i].c - arr[i].r;
+            ar -= ch;
+            if(ar < 0)break;
+            op++;
+
+        }
+        return op;
+        
+    }
+    class Pair{
+        int c;
+        int r;
+        Pair(int c, int r){
+            this.c = c;
+            this.r = r;
+        }
+    }
+}
