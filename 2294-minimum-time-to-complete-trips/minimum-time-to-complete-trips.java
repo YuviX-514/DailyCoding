c65a082452537b49f86bcecaa69b0362cde42c3b
Yuvraj Singh <yuvi4695@gmail.com>
2025-10-06 05:43:44 +0530

    Time: 53 ms (39.93%) | Memory: 59.2 MB (11.01%) - LeetSync

---
+++ 2294-minimum-time-to-complete-trips/minimum-time-to-complete-trips.java
@@ -0,0 +1,33 @@
+class Solution {
+
+    public static boolean isFeasible(int[]time, int totalTrips, long mid ){
+        long trips = 0;
+
+        for(int t : time){
+            trips += (mid)/t;
+        }
+        return trips >= totalTrips;
+    }
+    public long minimumTime(int[] time, int totalTrips) {
+        long start = time[0];
+        long end = 0;
+        
+        for(int t : time){
+            start = Math.min(t, start);
+        }
+        end = start* totalTrips;
+        long result = end;
+
+        while(start <= end){
+            long mid = start + (end - start)/2;
+            if(isFeasible(time, totalTrips, mid)){
+                result = mid;
+                end = mid - 1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        return result;
+    }
+}
