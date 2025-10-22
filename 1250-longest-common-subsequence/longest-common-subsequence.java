60975517b7a5e5072ea37b54fe1a74ce8b9f7b50
Yuvraj Singh <yuvi4695@gmail.com>
2025-10-22 17:49:57 +0530

    Time: 21 ms (52.71%) | Memory: 50.7 MB (84.96%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -14,8 +14,7 @@ class Solution {
                 else dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j]);
             }
         }
-        return dp[m][n];
-    }
+    
 
     // public int lcs(String s1, String s2, int i, int j, int[][]dp){
     //     if(i < 0 || j < 0)return 0;
@@ -24,5 +23,22 @@ class Solution {
     //     else return dp[i][j] = Math.max(lcs(s1, s2, i, j-1, dp), lcs(s1, s2, i-1, j, dp));
     // }
 
+    //print the lcs : 
 
+    StringBuilder sb = new StringBuilder();
+    int i = m, j = n;
+
+    while(i>0 && j>0){
+        if(text1.charAt(i-1) == text2.charAt(j-1)){
+            sb.append(text1.charAt(i-1));
+            i--; j--;
+        }
+        else if(dp[i-1][j] > dp[i][j-1])i--;
+        else j--;
+    }
+    sb.reverse();
+    System.out.println(sb);
+    return dp[m][n];
+    
+    }
 }
