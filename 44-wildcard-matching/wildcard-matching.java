42c737e737dc1ccfe42a0901e1beb9a0569c32cb
Yuvraj Singh <yuvi4695@gmail.com>
2025-11-04 12:52:47 +0530

    Time: 17 ms (73.06%) | Memory: 63.9 MB (5.00%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -1,22 +1,33 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-        Boolean dp[][] = new Boolean[s.length()+1][p.length()+1];
-        return fn(s, p, 0, 0, dp);
+        int m = s.length();
+        int n = p.length();
+
+        Boolean dp[][] = new Boolean[m][n];
+        // for(int d[] : dp)Arrays.fill(d, -1);
+        return fn(s, p, m-1, n-1, dp);
+
     }
 
     public boolean fn(String s, String p, int i, int j, Boolean[][]dp){
-        if(i == s.length() && j == p.length())return true;
-        
-        if(j == p.length())return false;
+        if(i < 0 && j < 0)return true; // both exhausted
+         
+        if(j < 0 && i >= 0)return false; //p phle h khatam hogyi 
+
+        if(i < 0 && j >= 0)return allStars(p, j);  //s phle khatm hogyi 
 
         if(dp[i][j] != null)return dp[i][j];
+        
+        if(s.charAt(i) == p.charAt(j) || p.charAt(j) == '?')return dp[i][j] = fn(s, p, i-1, j-1, dp);
+        else if(p.charAt(j) == '*')return dp[i][j] = fn(s, p, i-1, j, dp) || fn(s, p, i, j-1, dp);
 
-        boolean ans;
+        else return dp[i][j] = false;
+    }
 
-        if(p.charAt(j) == '*')ans = fn(s, p, i, j+1, dp) || (i<s.length() && fn(s, p, i+1, j, dp));
-        else if( (i < s.length())&& (s.charAt(i) == p.charAt(j) || p.charAt(j) == '?'))ans = fn(s, p, i+1, j+1, dp);
-        else ans = false;
-
-        return dp[i][j] = ans;
+    public boolean allStars(String p, int k){
+        for(int i = 0; i <=k;i++){
+            if(p.charAt(i) != '*')return false;
+        }
+        return true;
     }
 }
