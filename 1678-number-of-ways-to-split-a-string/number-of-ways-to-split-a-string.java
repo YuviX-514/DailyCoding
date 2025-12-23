1d2e6b262485a19c004fa00ed9736568359f2eb9
Yuvraj Singh <yuvi4695@gmail.com>
2025-12-23 10:54:20 +0530

    Time: 8 ms (78.08%) | Memory: 47.5 MB (65.07%) - LeetSync

---
+++ 1678-number-of-ways-to-split-a-string/number-of-ways-to-split-a-string.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int numWays(String s) {
+        long n=s.length();
+        long one=0;
+        long mod=1_000_000_007;
+        char[] c=s.toCharArray();
+        for(int i=0;i<c.length;i++)
+        {
+            if(c[i]=='1')
+                one++;
+        }
+        
+    
+        
+        if(one==0)
+        {
+            return (int)((((n-1)*(n-2))/2)%mod);
+        }
+        if(one%3!=0)
+            return 0;
+        //case 3
+        long ones=one/3;
+        one=0;
+        long part1=0;
+        long part2=0;
+        for(int i=0;i<c.length;i++)
+        {
+            if(c[i]=='1')
+                one++;
+            if(one==ones)
+                part1++;
+            if(one==2*ones)
+                part2++;
+        }
+        
+        return (int)((part1*part2)%mod);
+    }
+}
