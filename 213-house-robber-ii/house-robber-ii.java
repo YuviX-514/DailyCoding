3887a35f48165ed28304f6694535b63a34f7550c
Yuvraj Singh <yuvi4695@gmail.com>
2025-12-26 13:55:15 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (15.57%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,27 +1,25 @@
 class Solution {
     public int rob(int[] nums) {
-        int n= nums.length;
-
+        int n = nums.length;
         if(n == 1)return nums[0];
-        if(n == 2)return Math.max(nums[0], nums[1]);
-
-        int case1 = helper(nums, 0, n-2);
-        int case2 = helper(nums, 1, n-1);
-        return Math.max(case1, case2);
-    }
-    public int helper(int[] nums, int start, int end) {
-        int n =  end- start + 1;
-        int [] dp = new int[n];
-        dp[0] = nums[start];
         
-        for(int i = 1;i<n;i++){
-            int pick = nums[start + i];
-            if(i > 1)pick += dp[i-2];
-            int notPick = 0 + dp[i-1]; 
+        int dp[] = new int[nums.length];
+        Arrays.fill(dp, -1);
+        int a = h(0, n-2, nums, dp);
+        Arrays.fill(dp, -1);
+        int b = h(1, n-1, nums, dp);
 
-            dp[i] = Math.max(pick, notPick);
-        }
-        return dp[n-1];
+        return Math.max(a, b);
+    }
+
+    private int h (int i, int e, int[]nums, int[]dp){
+        if(i > e)return 0;
+
+        if(dp[i] != -1)return dp[i];
+
+        int p = nums[i] + h(i+2, e, nums, dp);
+        int np = h(i+1, e, nums, dp);
+
+        return dp[i] = Math.max(p, np);
     }
 }
-
